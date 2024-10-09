76bcb76d1f24cd837a735028594d6cf2ecba85f7
OPSolutions <david.merciales@opsolutions.biz>
2024-10-09 11:39:49 +0800

    Optimized Constants

--- app/src/main/java/com/example/androidexamtest/data/utils/Constants.kt
+++ app/src/main/java/com/example/androidexamtest/data/utils/Constants.kt
@@ -6,7 +6,6 @@ object Constants {
     const val VERSION = "v1"
     const val BASE_URL = "${PROTOCOL}://staging.api.primebmd.repnotes.com/${VERSION}"
 
-    const val LOGIN_TOKEN_DEV_REPNOTES = ""
     const val LOGIN_TOKEN_STG = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InByaW1lYm1kIiwiaWF0IjoxNjY4NTc4OTE0fQ.3QokvZxEbnN3bSnrURH7frAZu0MDW3snPxR2_7NiHPc"
     const val LOGIN_TOKEN_UAT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUHJpbWVCTUQiLCJpYXQiOjE1MTYyMzkwMjJ9.aEBhb-1kJlMQYCU7FqNCsjGGrzurkq6brLg0Y20bAKk"
 
